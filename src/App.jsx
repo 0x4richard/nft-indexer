9b5d8d51d90e58f72861be93f428a1fc7f46fa0f
Richard Hao <richard@digitalstar.tech>
2023-02-14 22:15:07 +0800

    feat: add environment variables to config the alchemy sdk

--- src/App.jsx
+++ src/App.jsx
@@ -8,46 +8,46 @@ import {
   Input,
   SimpleGrid,
   Text,
-} from '@chakra-ui/react';
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useState } from 'react';
+} from "@chakra-ui/react"
+import { Alchemy, Network } from "alchemy-sdk"
+import { useState } from "react"
 
 function App() {
-  const [userAddress, setUserAddress] = useState('');
-  const [results, setResults] = useState([]);
-  const [hasQueried, setHasQueried] = useState(false);
-  const [tokenDataObjects, setTokenDataObjects] = useState([]);
+  const [userAddress, setUserAddress] = useState("")
+  const [results, setResults] = useState([])
+  const [hasQueried, setHasQueried] = useState(false)
+  const [tokenDataObjects, setTokenDataObjects] = useState([])
 
   async function getNFTsForOwner() {
     const config = {
-      apiKey: '<-- COPY-PASTE YOUR ALCHEMY API KEY HERE -->',
+      apiKey: import.meta.env.VITE_ALCHEMY_API_KEY,
       network: Network.ETH_MAINNET,
-    };
+    }
 
-    const alchemy = new Alchemy(config);
-    const data = await alchemy.nft.getNftsForOwner(userAddress);
-    setResults(data);
+    const alchemy = new Alchemy(config)
+    const data = await alchemy.nft.getNftsForOwner(userAddress)
+    setResults(data)
 
-    const tokenDataPromises = [];
+    const tokenDataPromises = []
 
     for (let i = 0; i < data.ownedNfts.length; i++) {
       const tokenData = alchemy.nft.getNftMetadata(
         data.ownedNfts[i].contract.address,
         data.ownedNfts[i].tokenId
-      );
-      tokenDataPromises.push(tokenData);
+      )
+      tokenDataPromises.push(tokenData)
     }
 
-    setTokenDataObjects(await Promise.all(tokenDataPromises));
-    setHasQueried(true);
+    setTokenDataObjects(await Promise.all(tokenDataPromises))
+    setHasQueried(true)
   }
   return (
     <Box w="100vw">
       <Center>
         <Flex
-          alignItems={'center'}
+          alignItems={"center"}
           justifyContent="center"
-          flexDirection={'column'}
+          flexDirection={"column"}
         >
           <Heading mb={0} fontSize={36}>
             NFT Indexer 🖼
@@ -61,7 +61,7 @@ function App() {
         w="100%"
         flexDirection="column"
         alignItems="center"
-        justifyContent={'center'}
+        justifyContent={"center"}
       >
         <Heading mt={42}>Get all the ERC-721 tokens of this address:</Heading>
         <Input
@@ -80,14 +80,14 @@ function App() {
         <Heading my={36}>Here are your NFTs:</Heading>
 
         {hasQueried ? (
-          <SimpleGrid w={'90vw'} columns={4} spacing={24}>
+          <SimpleGrid w={"90vw"} columns={4} spacing={24}>
             {results.ownedNfts.map((e, i) => {
               return (
                 <Flex
-                  flexDir={'column'}
+                  flexDir={"column"}
                   color="white"
                   bg="blue"
-                  w={'20vw'}
+                  w={"20vw"}
                   key={e.id}
                 >
                   <Box>
@@ -95,15 +95,15 @@ function App() {
                   </Box>
                   <Image src={tokenDataObjects[i].rawMetadata.image} />
                 </Flex>
-              );
+              )
             })}
           </SimpleGrid>
         ) : (
-          'Please make a query! The query may take a few seconds...'
+          "Please make a query! The query may take a few seconds..."
         )}
       </Flex>
     </Box>
-  );
+  )
 }
 
-export default App;
+export default App
